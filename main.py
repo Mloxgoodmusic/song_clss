75d0b7528efbb15ae9aacdd36bb8a656c6e75c0b
mloxgoodmusic <mloxgoodmusic@gmail.com>
2022-11-16 01:24:28 +0200

    songs

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-class song:
+class Song:
     """class to represent a song"""
     '''attribute:
             title (str): the tittle of the song
@@ -18,4 +18,43 @@ class song:
         self.title = title
         self.artist = artist
         self.duration = duration
+
+
+class Album:
+    """ Class to represent an album, using its track title
+
+    Attributes:
+        album_name (str): The name of the album.
+        year (init): The year was album released.
+        artist (artist): The artist responsible for the album. if not specified,
+        the artist will default to an artist with the name "various artists".
+        tracks (list[song]): A list of the songs on the album.
+
+    Methods:
+        add_song: used to add a new song to the album's track list."""
+
+    def __init__(self, name, year, artist=None):
+        self.name = name
+        self.year = year
+        if artist is None:
+            self.artist = Artist("Various Artist")
+        else:
+            self.artist = artist
+
+        self.tracks = []
+
+    def add_song(self, song, position=None):
+        """ Adds a song to the track list
+
+        Args:
+            song(song): a song to add
+            position (optional): if specified, the song will be added to that position
+            in the track list - inserting it between other songs if necessary.
+            otherwise, the song will be added to the end of the list.
+        """
+        if position is None:
+            self.tracks.append(song)
+        else:
+            self.tracks.insert(position, song)
+
 
